747841c4a2f2834e83f5d8652994951a218dd1d0
MaxBernasconi <max.bernasconi86@gmail.com>
2023-05-15 14:32:04 -0300

    CV Started

---
+++ resources/views/livewire/cv-photo.blade.php
@@ -0,0 +1,3 @@
+<div class="relative rounded-md aspect-square overflow-hidden justify-center item-center ">
+    <img class="rounded absolute" src="{{ asset('storage/pf1.png') }}" alt="Max Bernasconi's Profile Photo">
+</div>
